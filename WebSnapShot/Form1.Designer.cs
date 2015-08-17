d8f5b90f4e5e4750a09815b5c2418c5828ee6e22
MJ.Pournassari <Mjpournassari@gmail.com>
2015-08-17 12:52:27 +0430

    * New Style

---
+++ WebSnapShot/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WebSnapShot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.groupBox2);
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(735, 395);
+            this.panel2.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(16, 50);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(464, 336);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Log";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(4, 20);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(456, 312);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(488, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(239, 386);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SnapShot";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 20);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(231, 362);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(16, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(460, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start Capture WebSite";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 395);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HTV Web V1.4 2015-08-17";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WebSnapShot/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+
+
+
+namespace WebSnapShot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public Bitmap GenerateScreenshot(string url, int width, int height)
+        {
+
+            WebBrowser wb = new WebBrowser();
+            wb.NewWindow += wb_NewWindow;
+            wb.ScrollBarsEnabled = false;
+            wb.ScriptErrorsSuppressed = true;
+            wb.Navigate(url);
+            while (wb.ReadyState != WebBrowserReadyState.Complete) { Application.DoEvents(); }
+
+
+            Thread.Sleep(int.Parse(ConfigurationSettings.AppSettings["WaitForLoadWebSite"].ToString().Trim()));
+
+
+            wb.Width = width;
+            wb.Height = height;
+
+            if (width == -1)
+            {
+
+                wb.Width = wb.Document.Body.ScrollRectangle.Width;
+            }
+
+            if (height == -1)
+            {
+
+                wb.Height = wb.Document.Body.ScrollRectangle.Height;
+            }
+
+            Bitmap bitmap = new Bitmap(wb.Width, wb.Height);
+            wb.DrawToBitmap(bitmap, new Rectangle(0, 0, wb.Width, wb.Height));
+            wb.Dispose();
+            return bitmap;
+        }
+
+        void wb_NewWindow(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            throw new NotImplementedException();
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int StartMinute = int.Parse(ConfigurationSettings.AppSettings["RenderIntervalMin"].ToString().Trim());
+            if (DateTime.Now.Minute == StartMinute)
+            {
+                button1_Click_1(new object(), new EventArgs());
+            }
+            //<add key="RenderHourEvenOdd"  value="odd"/>
+            //int Even = DateTime.Now.Hour % 2;
+            //if (ConfigurationSettings.AppSettings["RenderHourEvenOdd"].ToString().Trim().ToLower() == "even" && Even == 0)
+            //{
+            //    if (DateTime.Now.Minute >= StartMinute && DateTime.Now.Minute <= StartMinute + 1)
+            //    {
+            //        button1_Click_1(new object(), new EventArgs());
+            //    }
+            //}
+            //if (ConfigurationSettings.AppSettings["RenderHourEvenOdd"].ToString().Trim().ToLower() == "odd" && Even != 0)
+            //{
+            //    if (DateTime.Now.Minute >= StartMinute && DateTime.Now.Minute <= StartMinute + 1)
+            //    {
+            //        button1_Click_1(new object(), new EventArgs());
+            //    }
+            //}
+         
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DoMain();
+        }
+        protected void DoMain()
+        {
+            timer1.Enabled = false;
+            richTextBox1.Text = "";
+            button1.ForeColor = Color.White;
+            button1.Text = "Started";
+            button1.BackColor = Color.Red;
+
+
+            Bitmap thumbnail = GenerateScreenshot(ConfigurationSettings.AppSettings["WebUrl"].ToString().Trim(),
+               int.Parse(ConfigurationSettings.AppSettings["ImageWith"].ToString().Trim()),
+                int.Parse(ConfigurationSettings.AppSettings["ImageHeight"].ToString().Trim()));
+
+            Rectangle cropRect = new Rectangle(0,
+                int.Parse(ConfigurationSettings.AppSettings["ImagePaddingTop"].ToString().Trim()),
+                thumbnail.Width,
+                thumbnail.Height - int.Parse(ConfigurationSettings.AppSettings["ImagePaddingTop"].ToString().Trim()));
+
+            Bitmap target = new Bitmap(cropRect.Width, cropRect.Height);
+
+            using (Graphics g = Graphics.FromImage(target))
+            {
+                g.DrawImage(thumbnail, new Rectangle(0, 0, target.Width, target.Height),
+                                cropRect,
+                                GraphicsUnit.Pixel);
+            }
+            pictureBox1.Image = target;
+
+            target.Save(ConfigurationSettings.AppSettings["ImagePath"].ToString().Trim(),System.Drawing.Imaging.ImageFormat.Jpeg);
+            Application.DoEvents();
+
+            Process proc = new Process();
+            proc.StartInfo.FileName = "\"" + ConfigurationSettings.AppSettings["AeRenderPath"].ToString().Trim() + "\"";
+
+            try
+            {
+                string[] Dirsold = Directory.GetDirectories(ConfigurationSettings.AppSettings["OutputPath"].ToString().Trim());
+                foreach (var item in Dirsold)
+                {
+                    DirectoryInfo Dd = new DirectoryInfo(item);
+                    if (Dd.CreationTime.AddDays(3) < DateTime.Now)
+                        Dd.Delete(true);
+                }
+
+            }
+            catch
+            {
+
+            }
+
+
+            string FileName = ConfigurationSettings.AppSettings["OutPutFileName"].ToString().Trim() + "_" + string.Format("{0:0000}", DateTime.Now.Year) + "-" + string.Format("{0:00}", DateTime.Now.Month) + "-" + string.Format("{0:00}", DateTime.Now.Day) + "_" + string.Format("{0:00}", DateTime.Now.Hour) + "-" + string.Format("{0:00}", DateTime.Now.Minute) + "-" + string.Format("{0:00}", DateTime.Now.Second);
+            string Fol = ConfigurationSettings.AppSettings["OutputPath"].ToString().Trim() + string.Format("{0:0000}", DateTime.Now.Year) + "-" + string.Format("{0:00}", DateTime.Now.Month) + "-" + string.Format("{0:00}", DateTime.Now.Day) + "\\";
+
+            DirectoryInfo Dir = new DirectoryInfo(Fol);
+
+            if (!Dir.Exists)
+            {
+                Dir.Create();
+            }
+
+
+            proc.StartInfo.Arguments = " -project " + "\"" + ConfigurationSettings.AppSettings["AeProjectFile"].ToString().Trim() + "\"" + "   -comp   \"" + ConfigurationSettings.AppSettings["Composition"].ToString().Trim() + "\" -output " + "\"" + Fol + FileName + ".mp4" + "\"";
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.EnableRaisingEvents = true;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
+
+            if (!proc.Start())
+            {
+                return;
+            }
+
+            proc.PriorityClass = ProcessPriorityClass.Normal;
+            StreamReader reader = proc.StandardOutput;
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                richTextBox1.Text += (line) + " \n";
+                richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                richTextBox1.ScrollToCaret();
+                Application.DoEvents();
+            }
+            proc.Close();
+            thumbnail.Dispose();
+
+            
+            button1.ForeColor = Color.White;
+            button1.Text = "Start";
+            button1.BackColor = Color.Navy;
+            timer1.Enabled = true;
+        }
+    }
+}
