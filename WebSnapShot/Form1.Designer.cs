5f3674ba970cb96ec02e9551e4d773e9e98705a7
MJ.Pournassari <Mjpournassari@gmail.com>
2016-04-02 17:16:07 +0430

    Revert "API"
    This reverts commit 26312ca2e136f73f6fdaa3135718e4400b81fe2e.

--- WebSnapShot/Form1.Designer.cs
+++ WebSnapShot/Form1.Designer.cs
@@ -56,20 +56,20 @@
             this.panel2.Controls.Add(this.button1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(827, 494);
+            this.panel2.Size = new System.Drawing.Size(735, 395);
             this.panel2.TabIndex = 2;
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.richTextBox1);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(18, 62);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Location = new System.Drawing.Point(16, 50);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox2.Size = new System.Drawing.Size(522, 420);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(464, 336);
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Log";
@@ -80,10 +80,10 @@
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox1.ForeColor = System.Drawing.Color.White;
-            this.richTextBox1.Location = new System.Drawing.Point(4, 24);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox1.Location = new System.Drawing.Point(4, 20);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(514, 391);
+            this.richTextBox1.Size = new System.Drawing.Size(456, 312);
             this.richTextBox1.TabIndex = 4;
             this.richTextBox1.Text = "";
             // 
@@ -91,11 +91,11 @@
             // 
             this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(549, 5);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Location = new System.Drawing.Point(488, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox1.Size = new System.Drawing.Size(269, 482);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(239, 386);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "SnapShot";
@@ -104,10 +104,10 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(4, 24);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Location = new System.Drawing.Point(4, 20);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(261, 453);
+            this.pictureBox1.Size = new System.Drawing.Size(231, 362);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -118,10 +118,10 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(18, 5);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Location = new System.Drawing.Point(16, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(518, 49);
+            this.button1.Size = new System.Drawing.Size(460, 39);
             this.button1.TabIndex = 3;
             this.button1.Text = "Start Capture WebSite";
             this.button1.UseVisualStyleBackColor = false;
@@ -129,16 +129,16 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(827, 494);
+            this.ClientSize = new System.Drawing.Size(735, 395);
             this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "HTV Web V1.4 20160130";
+            this.Text = "HTV Web V1.4 2015-08-17";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel2.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
--- WebSnapShot/Form1.cs
+++ WebSnapShot/Form1.cs
@@ -10,7 +10,9 @@ using System.Configuration;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
-using System.Net;
+
+
+
 
 namespace WebSnapShot
 {
@@ -20,8 +22,56 @@ namespace WebSnapShot
         {
             InitializeComponent();
         }
+
         private void Form1_Load(object sender, EventArgs e)
-        { }
+        {
+
+        }
+
+
+        public Bitmap GenerateScreenshot(string url, int width, int height)
+        {
+
+            WebBrowser wb = new WebBrowser();
+            wb.NewWindow += wb_NewWindow;
+            wb.ScrollBarsEnabled = false;
+            wb.ScriptErrorsSuppressed = true;
+            wb.Navigate(url);
+            while (wb.ReadyState != WebBrowserReadyState.Complete) { Application.DoEvents(); }
+
+
+            Thread.Sleep(int.Parse(ConfigurationSettings.AppSettings["WaitForLoadWebSite"].ToString().Trim()));
+
+
+            wb.Width = width;
+            wb.Height = height;
+
+            if (width == -1)
+            {
+
+                wb.Width = wb.Document.Body.ScrollRectangle.Width;
+            }
+
+            if (height == -1)
+            {
+
+                wb.Height = wb.Document.Body.ScrollRectangle.Height;
+            }
+
+            Bitmap bitmap = new Bitmap(wb.Width, wb.Height);
+            wb.DrawToBitmap(bitmap, new Rectangle(0, 0, wb.Width, wb.Height));
+            wb.Dispose();
+            return bitmap;
+        }
+
+        void wb_NewWindow(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            throw new NotImplementedException();
+        }
+
+
+
         private void timer1_Tick(object sender, EventArgs e)
         {
             int StartMinute = int.Parse(ConfigurationSettings.AppSettings["RenderIntervalMin"].ToString().Trim());
@@ -29,7 +79,26 @@ namespace WebSnapShot
             {
                 button1_Click_1(new object(), new EventArgs());
             }
+            //<add key="RenderHourEvenOdd"  value="odd"/>
+            //int Even = DateTime.Now.Hour % 2;
+            //if (ConfigurationSettings.AppSettings["RenderHourEvenOdd"].ToString().Trim().ToLower() == "even" && Even == 0)
+            //{
+            //    if (DateTime.Now.Minute >= StartMinute && DateTime.Now.Minute <= StartMinute + 1)
+            //    {
+            //        button1_Click_1(new object(), new EventArgs());
+            //    }
+            //}
+            //if (ConfigurationSettings.AppSettings["RenderHourEvenOdd"].ToString().Trim().ToLower() == "odd" && Even != 0)
+            //{
+            //    if (DateTime.Now.Minute >= StartMinute && DateTime.Now.Minute <= StartMinute + 1)
+            //    {
+            //        button1_Click_1(new object(), new EventArgs());
+            //    }
+            //}
+         
         }
+
+
         private void button1_Click_1(object sender, EventArgs e)
         {
             DoMain();
@@ -41,17 +110,33 @@ namespace WebSnapShot
             button1.ForeColor = Color.White;
             button1.Text = "Started";
             button1.BackColor = Color.Red;
-            try
-            {
-                WebClient webClient = new WebClient();
-                webClient.DownloadFile(ConfigurationSettings.AppSettings["WebUrl"].ToString().Trim() + "&dt=" + DateTime.Now.ToString("yyyyMMddhhmmss"), ConfigurationSettings.AppSettings["ImagePath"].ToString().Trim());
 
+
+            Bitmap thumbnail = GenerateScreenshot(ConfigurationSettings.AppSettings["WebUrl"].ToString().Trim(),
+               int.Parse(ConfigurationSettings.AppSettings["ImageWith"].ToString().Trim()),
+                int.Parse(ConfigurationSettings.AppSettings["ImageHeight"].ToString().Trim()));
+
+            Rectangle cropRect = new Rectangle(0,
+                int.Parse(ConfigurationSettings.AppSettings["ImagePaddingTop"].ToString().Trim()),
+                thumbnail.Width,
+                thumbnail.Height - int.Parse(ConfigurationSettings.AppSettings["ImagePaddingTop"].ToString().Trim()));
+
+            Bitmap target = new Bitmap(cropRect.Width, cropRect.Height);
+
+            using (Graphics g = Graphics.FromImage(target))
+            {
+                g.DrawImage(thumbnail, new Rectangle(0, 0, target.Width, target.Height),
+                                cropRect,
+                                GraphicsUnit.Pixel);
             }
-            catch { }
-            pictureBox1.Image = Image.FromFile(ConfigurationSettings.AppSettings["ImagePath"].ToString().Trim());
+            pictureBox1.Image = target;
+
+            target.Save(ConfigurationSettings.AppSettings["ImagePath"].ToString().Trim(),System.Drawing.Imaging.ImageFormat.Jpeg);
             Application.DoEvents();
+
             Process proc = new Process();
             proc.StartInfo.FileName = "\"" + ConfigurationSettings.AppSettings["AeRenderPath"].ToString().Trim() + "\"";
+
             try
             {
                 string[] Dirsold = Directory.GetDirectories(ConfigurationSettings.AppSettings["OutputPath"].ToString().Trim());
@@ -61,14 +146,25 @@ namespace WebSnapShot
                     if (Dd.CreationTime.AddDays(3) < DateTime.Now)
                         Dd.Delete(true);
                 }
+
             }
             catch
-            { }
+            {
+
+            }
+
+
             string FileName = ConfigurationSettings.AppSettings["OutPutFileName"].ToString().Trim() + "_" + string.Format("{0:0000}", DateTime.Now.Year) + "-" + string.Format("{0:00}", DateTime.Now.Month) + "-" + string.Format("{0:00}", DateTime.Now.Day) + "_" + string.Format("{0:00}", DateTime.Now.Hour) + "-" + string.Format("{0:00}", DateTime.Now.Minute) + "-" + string.Format("{0:00}", DateTime.Now.Second);
             string Fol = ConfigurationSettings.AppSettings["OutputPath"].ToString().Trim() + string.Format("{0:0000}", DateTime.Now.Year) + "-" + string.Format("{0:00}", DateTime.Now.Month) + "-" + string.Format("{0:00}", DateTime.Now.Day) + "\\";
+
             DirectoryInfo Dir = new DirectoryInfo(Fol);
+
             if (!Dir.Exists)
-            { Dir.Create(); }
+            {
+                Dir.Create();
+            }
+
+
             proc.StartInfo.Arguments = " -project " + "\"" + ConfigurationSettings.AppSettings["AeProjectFile"].ToString().Trim() + "\"" + "   -comp   \"" + ConfigurationSettings.AppSettings["Composition"].ToString().Trim() + "\" -output " + "\"" + Fol + FileName + ".mp4" + "\"";
             proc.StartInfo.RedirectStandardError = true;
             proc.StartInfo.UseShellExecute = false;
@@ -76,8 +172,12 @@ namespace WebSnapShot
             proc.EnableRaisingEvents = true;
             proc.StartInfo.RedirectStandardOutput = true;
             proc.StartInfo.RedirectStandardError = true;
+
             if (!proc.Start())
-            { return; }
+            {
+                return;
+            }
+
             proc.PriorityClass = ProcessPriorityClass.Normal;
             StreamReader reader = proc.StandardOutput;
             string line;
@@ -89,6 +189,9 @@ namespace WebSnapShot
                 Application.DoEvents();
             }
             proc.Close();
+            thumbnail.Dispose();
+
+            
             button1.ForeColor = Color.White;
             button1.Text = "Start";
             button1.BackColor = Color.Navy;
